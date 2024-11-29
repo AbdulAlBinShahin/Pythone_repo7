b5573383a0a5d15ca889d2d705f10f43c2a314ae
AbdulAlBinShahin <abdulalbinshahin@gmail.com>
2024-11-30 00:33:03 +0600

    Uploaded

---
+++ Merge_2_Dictionary.py
@@ -0,0 +1,6 @@
+my_dict={'name':'shahin','age':22,'area':'ctg'}
+my_dict2={'name1':'shahin1','age1':221,'area1':'ctg1'}
+ 
+my_dict.update(my_dict2)
+print(f"{my_dict}")
+print("Merge Dictionary :",dictionary1)
